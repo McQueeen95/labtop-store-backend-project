3be5b5400193bfc98648bd818f6e4aabf25564c4
Ahmed Mohamed <ahmedsuberyahooman2592002@gmail.com>
2024-12-21 08:14:14 +0200

    update

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-const { connectDB, app, express } = require('./config/db.js');
+// const { connectDB, app, express } = require('../config/db.js');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const DB_URI = process.env.DB_URI;
+const port = process.env.PORT;
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB_URI, clientOptions);
+    console.log('Connected to MongoDB');
+    app.listen(port, () => {
+      console.log(`app is listening on port ${port} locally`);
+    });
+  } catch (error) {
+    console.log('Error connecting to MongoDB:', error.message);
+    process.exit(1);
+  }
+}
+
+// module.exports = { app, express, connectDB };
 const cors = require('cors');
 const helmet = require('helmet');
 const userRoutes = require('./routes/userRoutes.js');
-const laptopRoutes = require('./routes/laptopRoutes')
-const categoryRoutes = require('./routes/categoryRoutes')
-const brandRoutes = require('./routes/brandRoutes')
-const authRoutes = require('./routes/authRoutes')
-const rateRoutes = require('./routes/rateRoutes')
-const cartRoutes = require('./routes/cartRoutes')
-const cartItemRoutes = require('./routes/cartItemRoutes')
+const laptopRoutes = require('./routes/laptopRoutes.js')
+const categoryRoutes = require('./routes/categoryRoutes.js')
+const brandRoutes = require('./routes/brandRoutes.js')
+const authRoutes = require('./routes/authRoutes.js')
+const rateRoutes = require('./routes/rateRoutes.js')
+const cartRoutes = require('./routes/cartRoutes.js')
+const cartItemRoutes = require('./routes/cartItemRoutes.js')
 
 
 app.use(cors());
@@ -25,17 +49,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.all('*', (req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-app.use('/user', userRoutes)
-app.use('/auth', authRoutes)
-app.use('/laptop', laptopRoutes)
-app.use('/category', categoryRoutes)
-app.use('/brand', brandRoutes)
-app.use('/rate', rateRoutes)
-app.use('/cart', cartRoutes)
-app.use('/cartItem', cartItemRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/laptop', laptopRoutes)
+app.use('/api/category', categoryRoutes)
+app.use('/api/brand', brandRoutes)
+app.use('/api/rate', rateRoutes)
+app.use('/api/cart', cartRoutes)
+app.use('/api/cartItem', cartItemRoutes)
 
 connectDB();
